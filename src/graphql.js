7592963c6259a8fe4d0ad3bdb5e5f1a2befe4c81
Corpse203 <52112663+Corpse203@users.noreply.github.com>
2025-09-18 15:11:05 +0200

    Update graphql.js

--- src/graphql.js
+++ src/graphql.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+// Endpoint GraphQL HTTP usuel côté DLive
 const GRAPHQL_HTTP = 'https://graphigo.prd.dlive.tv/';
 
 export async function postGraphQL(query, variables, accessToken) {
@@ -14,7 +15,9 @@ export async function postGraphQL(query, variables, accessToken) {
   return res.data.data;
 }
 
+/** Envoi d’un message dans le chat (mutation HTTP, token UTILISATEUR) */
 export async function sendChatMessage(streamerUsername, message, userAccessToken) {
+  // Variante 1 fréquemment exposée
   const m1 = `
     mutation($streamer: String!, $message: String!) {
       sendChatMessage(streamer: $streamer, message: $message) { id }
@@ -23,6 +26,7 @@ export async function sendChatMessage(streamerUsername, message, userAccessToken
   try {
     return await postGraphQL(m1, { streamer: streamerUsername, message }, userAccessToken);
   } catch (e) {
+    // Variante 2 (fallback)
     const m2 = `
       mutation($streamer: String!, $message: String!) {
         sendStreamMessage(streamer: $streamer, message: $message) { id }
